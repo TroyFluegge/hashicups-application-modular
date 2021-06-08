cb3f9592fd24683cbaee5807ca7f42973f90ea3a
TroyFluegge <troy@hashicorp.com>
2021-06-07 20:51:46 -0500

    data resource issues

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,28 @@
 output "environment" {
     value = var.environment
 }
+
 output "sshkey" {
   sensitive = true
   value = tls_private_key.awskey.private_key_pem
 }
+
 output "hashicups-ui" {
   value = "http://${module.frontend.public_ip}"
 }
+
 output "frontend-ip" {
   value =   module.frontend.public_ip
 }
+
 output "public-api" {
   value =   module.public_api.public_ip
 }
+
 output "product-api" {
   value =   module.product_api.public_ip
 }
+
 output "postgres-api" {
   value =   module.postgres.public_ip
 }

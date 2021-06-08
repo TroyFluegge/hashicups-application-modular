a94b58f9df6a279c1142ade1f46713388254dace
TroyFluegge <troy@hashicorp.com>
2021-06-07 20:42:00 -0500

    data resource issues

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,9 @@
 output "output" {
   value = <<README
-
 frontend: ssh -i ~/.ssh/awskey-modular.pem centos@${module.frontend.public_ip}
 public-api: ssh -i ~/.ssh/awskey-modular.pem centos@${module.public_api.public_ip}
 product-api: ssh -i ~/.ssh/awskey-modular.pem centos@${module.product_api.public_ip}
 postgres: ssh -i ~/.ssh/awskey-modular.pem centos@${module.postgres.public_ip}
-
-Takes a while for docker:
-http://${module.frontend.public_ip}
-
 README
 }
 
@@ -19,4 +14,8 @@ output "environment" {
 output "sshkey" {
   sensitive = true
   value = tls_private_key.awskey.private_key_pem
+}
+
+output "hashicups-ui" {
+  value = "http://${module.frontend.public_ip}"
 }

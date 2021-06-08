04f6150fd93d2261eb0caaa4793f861aee72839d
TroyFluegge <troy@hashicorp.com>
2021-06-07 20:45:53 -0500

    data resource issues

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,22 @@
-output "output" {
-  value = <<README
-frontend: ssh -i ~/.ssh/awskey-modular.pem centos@${module.frontend.public_ip}
-public-api: ssh -i ~/.ssh/awskey-modular.pem centos@${module.public_api.public_ip}
-product-api: ssh -i ~/.ssh/awskey-modular.pem centos@${module.product_api.public_ip}
-postgres: ssh -i ~/.ssh/awskey-modular.pem centos@${module.postgres.public_ip}
-README
-}
-
 output "environment" {
     value = var.environment
 }
-
 output "sshkey" {
   sensitive = true
   value = tls_private_key.awskey.private_key_pem
 }
-
 output "hashicups-ui" {
   value = "http://${module.frontend.public_ip}"
+}
+output "frontend-ip" {
+  value =   module.frontend.public_ip
+}
+output "public-api" {
+  value =   module.public_api.public_ip
+}
+output "product-api" {
+  value =   module.product_api.public_ip
+}
+output "postgres-api" {
+  value =   module.postgres.public_ip
 }

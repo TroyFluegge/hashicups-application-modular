e05e29b449a1c21d6267e646bd7603b9b5ee3c20
TroyFluegge <troy@hashicorp.com>
2021-06-07 20:29:03 -0500

    data resource issues

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 output "output" {
-  sensitive = true
   value = <<README
 
 frontend: ssh -i ~/.ssh/awskey-modular.pem centos@${module.frontend.public_ip}
@@ -10,12 +9,14 @@ postgres: ssh -i ~/.ssh/awskey-modular.pem centos@${module.postgres.public_ip}
 Takes a while for docker:
 http://${module.frontend.public_ip}
 
-ssh-key (save as ~/.ssh/awskey-modular.pem)
-${tls_private_key.awskey.private_key_pem}
-
 README
 }
 
 output "environment" {
     value = var.environment
+}
+
+output "sshkey" {
+  sensitive = true
+  value = tls_private_key.awskey.private_key_pem
 }
